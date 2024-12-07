f1231c7b4a0e02b828f90171546d6087817ecc16
Sebastian Flum <sebastian.flum.dev@gmail.com>
2024-12-07 15:34:58 +0100

    working now

--- discord_bot/music_player.py
+++ discord_bot/music_player.py
@@ -336,7 +336,6 @@ class YouTubeDownloader(discord.PCMVolumeTransformer):
         # Create absolute path to cookie file
         root_dir = os.path.dirname(os.path.abspath(__file__))
         cookiefile_path = os.path.join(root_dir, "..", 'cookies.txt')
-        print(cookiefile_path)
 
         ytl_options = {
             "format": "bestaudio/best",
